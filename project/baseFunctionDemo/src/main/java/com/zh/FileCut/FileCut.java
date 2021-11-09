3dc126a35842cb01f42be2bfc2aaaddf8ba26ea9
zhangyuhan <1355166049@qq.com>
2021-11-09 14:04:23 +0800

    Optional完成

---
+++ project/baseFunctionDemo/src/main/java/com/zh/FileCut/FileCut.java
@@ -0,0 +1,38 @@
+package com.zh.FileCut;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+/**
+ * @ClassName FileCut
+ * @Description 图片的剪切
+ * @Author Zhangyuhan
+ * @Date 2021/11/9
+ * @Version 1.0
+ */
+public class FileCut {
+    @Test
+    public void test() {
+        System.out.println(cut(new File("D:\\wendang\\图片\\pixiv\\ATDAN-竖屏\\56480020_p0.jpg"),
+                "D:\\wendang\\图片\\pixiv\\ATDAN-横屏"));
+    }
+
+    public static boolean cut(File file, String destDir) {
+        String fileName = file.getName();
+        byte[] bytes = new byte[1024];
+        int readLen = -1;
+        try (FileInputStream in = new FileInputStream(file);
+             FileOutputStream out = new FileOutputStream(destDir + "/" + fileName)) {
+            while ((readLen = in.read(bytes)) != -1) {
+                out.write(bytes, 0, readLen);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return file.delete();
+    }
+}
---
+++ project/baseFunctionDemo/src/main/java/com/zh/work_demo/_5获取图片的长宽.java
@@ -0,0 +1,118 @@
+package com.zh.work_demo;
+
+import com.zh.FileCut.FileCut;
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * @ClassName _5获取图片的长宽
+ * @Description TODO
+ * @Author Zhangyuhan
+ * @Date 2021/11/9
+ * @Version 1.0
+ */
+public class _5获取图片的长宽 {
+    @Test
+    public void test() {
+        String sourceDir = "D:\\wendang\\图片\\pixiv\\米山舞";
+        String verDir = "D:\\wendang\\图片\\pixiv\\米山舞-竖屏";
+        String horDir = "D:\\wendang\\图片\\pixiv\\米山舞-横屏";
+
+        File file = new File(sourceDir);
+        File[] files = file.listFiles();
+        for (File tempFile : files) {
+            int[] imgWidthAndHeight = getImgWidthAndHeight(tempFile);
+            if (imgWidthAndHeight[0] > imgWidthAndHeight[1]) {// 宽度大于高度
+                boolean cut = FileCut.cut(tempFile, horDir);
+                if (cut) {
+                    System.out.println("移动" + tempFile.getName() + "成功");
+                }
+            }else {
+                boolean cut = FileCut.cut(tempFile, verDir);
+                if (cut) {
+                    System.out.println("移动" + tempFile.getName() + "成功");
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取图片宽度
+     *
+     * @param file 图片文件
+     * @return 宽度
+     */
+    public static int getImgWidth(File file) {
+        InputStream is = null;
+        BufferedImage src = null;
+        int ret = -1;
+        try {
+            is = new FileInputStream(file);
+            src = javax.imageio.ImageIO.read(is);
+            ret = src.getWidth(null); // 得到源图宽
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+
+    /**
+     * 获取图片高度
+     *
+     * @param file 图片文件
+     * @return 高度
+     */
+    public static int getImgHeight(File file) {
+        InputStream is = null;
+        BufferedImage src = null;
+        int ret = -1;
+        try {
+            is = new FileInputStream(file);
+            src = javax.imageio.ImageIO.read(is);
+            ret = src.getHeight(null); // 得到源图高
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    /**
+     * 获取图片的宽度和高度
+     *
+     * @param file
+     * @return
+     */
+    public static int[] getImgWidthAndHeight(File file) {
+        InputStream is = null;
+        BufferedImage src = null;
+        int[] res = {-1, -1};
+        try {
+            is = new FileInputStream(file);
+            src = javax.imageio.ImageIO.read(is);
+            res[0] = src.getWidth(null);// 得到图片的宽度
+            res[1] = src.getHeight(null); // 得到源图高度
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return res;
+    }
+}
