5a208993053fdb7ef98f15ac940f54d15a88ce8d
zhangyuhan <1355166049@qq.com>
2021-12-21 09:29:12 +0800

    vue update

--- part/vue/learnvue-06-vuex/src/App.vue
+++ part/vue/learnvue-06-vuex/src/App.vue
@@ -4,7 +4,7 @@
     <h2>{{ counter }}</h2>
     <button @click="counter++">+</button>
     <button @click="counter--">-</button>
-    <hr>
+    <hr />
     <!--    注意：不要像下面那样直接修改state,而是要按照官方-->
     <!--    文档说的那样通过mutation来修改state-->
     <!--    原因：直接修改state，devtools无法跟踪到是哪个组件在执行相关操作-->
@@ -13,13 +13,18 @@
     <button @click="$store.state.shareCounter--">-</button>
     <button @click="addtion">通过mutation修改counter+</button>
     <button @click="subtraction">通过mutation修改counter-</button>
+
+    <!-- mutation提交携带参数 -->
+    <button @click="addCount(5)">+5</button>
+    <button @click="addCount(10)">+10</button>
+    <button @click="addStudent">增加学生</button>
     <h2>{{ $store.state.shareCounter }}</h2>
     <!--    如果想要获得shareCounter的平方-->
     <!--    这种写法也可以，但是一它很丑，二它不会缓存结果，有一个重新计算一遍，三多个组件用，多个组件要写重复的很丑的代码-->
     <h2>{{ $store.state.shareCounter * $store.state.shareCounter }}</h2>
     <!--    通过vuex的getters来获取-->
     <h2>{{ $store.getters.powSharedCounter }}</h2>
-    <hr>
+    <hr />
     <hello-vuex :counter="counter"></hello-vuex>
   </div>
 </template>
@@ -28,27 +33,45 @@
 import HelloVuex from "./components/HelloVuex";
 
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
-      message: '我是app组件',
+      message: "我是app组件",
       counter: 0,
-    }
+    };
   },
   methods: {
     addtion() {
-      this.$store.commit('increment')
+      this.$store.commit("increment");
     },
     subtraction() {
-      this.$store.commit('decrement')
-    }
+      this.$store.commit("decrement");
+    },
+    // 修改state通过commit,并携带参数
+    // 形如下面的参数count或者stu，在术语上称之为mutation的负载（载荷payload)。
+    // 如果参数不是一个的话，payload也可以是一个对象，这个时候在对象中提取相关的参数
+    addCount(count) {
+      // 1.下面是普通的提交风格，除了这种普通的提交风格之外，vue还提供了一种对象的提交风格
+      // this.$store.commit("incrementCount", count);
+
+      // 2.对象的提交风格
+      this.$store.commit({
+        type: "incrementCount", // type就是提交类型（事件类型）
+        // 其他的多个参数可以依次罗列在下面，但是注意，事件类型里接收参数的方式和普通的提交方式不太一样。详见store里对应的commit
+        count,
+        age: 18,
+      });
+    },
+    addStudent() {
+      let stu = { id: 114, name: "alan", age: 35 };
+      this.$store.commit("addStudent", stu);
+    },
   },
   components: {
-    HelloVuex
-  }
-}
+    HelloVuex,
+  },
+};
 </script>
 
 <style>
-
 </style>
--- part/vue/learnvue-06-vuex/src/store/index.js
+++ part/vue/learnvue-06-vuex/src/store/index.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex"
 
 // 1.安装插件
-Vue.use(Vuex);// 底层执行Vuex.install()
+Vue.use(Vuex); // 底层执行Vuex.install()
 
 // 2.创建对象
 // 关于vuex单一状态树(单一数据源)的理解：
@@ -12,20 +12,38 @@ const store = new Vuex.Store({
   state: {
     shareCounter: 1000,
     students: [
-      {id: 110, name: 'why', age: 18},
-      {id: 111, name: 'kobe', age: 24},
-      {id: 112, name: 'james', age: 30},
-      {id: 113, name: 'curry', age: 10},
+      { id: 110, name: 'why', age: 18 },
+      { id: 111, name: 'kobe', age: 24 },
+      { id: 112, name: 'james', age: 30 },
+      { id: 113, name: 'curry', age: 10 },
     ]
   },
   mutations: {
-    //  方法
-    //  默认把state传进去
+    // 方法
+    // 默认把state传进去
     increment(state) {
       state.shareCounter++
     },
     decrement(state) {
       state.shareCounter--
+    },
+    incrementCount(state, count) {
+      // 1.普通的提交风格这里的count就是commit里的第二个参数(5,10.....)
+      // console.log(count);
+      // state.shareCounter += count
+
+      // 2.对象的提交风格。这里的count是形如下面的对象（就是commit多提交的对象)
+      // {
+      //   type:'incrementCount',
+      //   count:10,
+      //   age:18,
+      // }
+      // 这种情况下，建议把上面的第二个参数count直接改名为payload(载荷)更合适
+      console.log(count);
+      state.shareCounter += count.count
+    },
+    addStudent(state, stu) {
+      state.students.push(stu)
     }
   },
   actions: {},
