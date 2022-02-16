88afd2da00ee1f71196e2408dea4d774dca2e991
zhangyuhan <1355166049@qq.com>
2022-02-16 15:36:59 +0800

    日常提交

--- part/vue/learnvue-08-project-mall/src/main.js
+++ part/vue/learnvue-08-project-mall/src/main.js
@@ -5,6 +5,7 @@ import router from "./router";
 // import 'element-ui/lib/theme-chalk/index.css';
 import store from "./store";
 import toast from "components/common/toast";
+import FastClick from 'fastclick'
 // Vue.use(ElementUI);
 // 安装toast插件
 Vue.use(toast)
@@ -12,6 +13,8 @@ Vue.use(toast)
 Vue.config.productionTip = false
 // 将新的Vue实例作为事件总线
 Vue.prototype.$bus = new Vue()
+// 通过fastclick解决移动端点击的300ms延迟
+FastClick.attach(document.body)
 
 new Vue({
   router,
