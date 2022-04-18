de369c7dcc96f6a97f5f257a1098b1496c57f79e
zhangyuhan <1355166049@qq.com>
2022-04-18 10:15:16 +0800

    springboot2 update

--- part/springboot2/src/main/java/com/study/springboot2study/config/WebConfig.java
+++ part/springboot2/src/main/java/com/study/springboot2study/config/WebConfig.java
@@ -6,14 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.util.StringUtils;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+import org.springframework.web.accept.ParameterContentNegotiationStrategy;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.UrlPathHelper;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @ClassName WebConfig
@@ -79,6 +83,28 @@ public class WebConfig {
             @Override
             public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
                 converters.add(new GuiguMessageConvertor());
+            }
+
+            // 自定义内容协商策略，这样浏览器访问的时候加个参数format=gg,就会返回以x-gui协议写出的内容
+            @Override
+            public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+                // 基于参数的内容协商管理器
+                Map<String, MediaType> mediaTypes = new HashMap<>();
+                // 指定支持解析哪些参数对应的哪些媒体类型
+                mediaTypes.put("json", MediaType.APPLICATION_JSON);
+                mediaTypes.put("xml", MediaType.APPLICATION_XML);
+                mediaTypes.put("gg", MediaType.parseMediaType("application/x-guigu"));
+                ParameterContentNegotiationStrategy parameterStrategy = new ParameterContentNegotiationStrategy(mediaTypes);
+                // parameterStrategy.setParameterName("ff");// 默认是format,可以手动更改基于参数的内容协商的参数名称
+
+                // 基于请求头的内容协商管理器
+                // 这里之所以要放这个基于请求头的内容协商管理器，是因为此时自定义的内容协商管理器覆盖了系统默认的。
+                // 此时整个容器中若不放基于请求头的内容协商管理器的话，就只剩下了基于参数的内容协商管理器。
+                // 那么就会出现无论请求头的accept字段传什么值，都只能找到*/*,即返回默认的json
+                HeaderContentNegotiationStrategy headerStrategy = new HeaderContentNegotiationStrategy();
+
+                configurer.strategies(Arrays.asList(parameterStrategy, headerStrategy));
             }
         };
     }
