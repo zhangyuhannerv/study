c7d752a03ee30859d173d59e5d306ffe4f2b1ca7
zhangyuhan <1355166049@qq.com>
2022-05-24 18:09:09 +0800

    rabbitmq update

---
+++ part/rabbitmq/rabbitmq-hello/src/main/java/com/study/rabbitmq/_5Exchange/topic/EmitLogTopic.java
@@ -0,0 +1,39 @@
+package com.study.rabbitmq._5Exchange.topic;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.study.rabbitmq.utils.RabbitMqUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmitLogTopic {
+    public static final String EXCHANGE_NAME = "topic_logs";
+
+    public static void main(String[] args) throws Exception {
+        Channel channel = RabbitMqUtils.getChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+
+        /*
+         * 两个队列绑定的key值说明：
+         * Q1-->*.orange.*
+         * Q2-->*.*.rabbit
+         * Q2-->lazy.#
+         */
+        Map<String, String> bindingKeyMap = new HashMap<>();
+        bindingKeyMap.put("quick.orange.rabbit", "被队列 Q1Q2 接收到");
+        bindingKeyMap.put("lazy.orange.elephant", "被队列 Q1Q2 接收到");
+        bindingKeyMap.put("quick.orange.fox", "被队列 Q1 接收到");
+        bindingKeyMap.put("lazy.brown.fox", "被队列 Q2 接收到");
+        bindingKeyMap.put("lazy.pink.rabbit", "虽然满足两个绑定但只被队列 Q2 接收一次");
+        bindingKeyMap.put("quick.brown.fox", "不匹配任何绑定不会被任何队列接收到会被丢弃");
+        bindingKeyMap.put("quick.orange.male.rabbit", "是四个单词不匹配任何绑定会被丢弃");
+        bindingKeyMap.put("lazy.orange.male.rabbit", "是四个单词但匹配 Q2");
+
+        // 循环发送消息
+        for (Map.Entry<String, String> entry : bindingKeyMap.entrySet()) {
+            channel.basicPublish(EXCHANGE_NAME, entry.getKey(), null, entry.getValue().getBytes());
+            System.out.println("生产者发出消息：" + entry.getValue());
+        }
+    }
+}
