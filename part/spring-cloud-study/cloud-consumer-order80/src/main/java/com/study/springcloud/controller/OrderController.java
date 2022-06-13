f7aa2c1b39eba932aae5f1191ab31593f47086c4
zhangyuhan <1355166049@qq.com>
2022-06-13 09:21:25 +0800

    spring-cloud update

---
+++ part/spring-cloud-study/cloud-consumer-order80/src/main/java/com/study/springcloud/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.study.springcloud.controller;
+
+import com.study.springcloud.entities.CommonResult;
+import com.study.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/consumer")
+@Slf4j
+public class OrderController {
+    public static final String PAYMENT_URL = "http://127.0.0.1:8001";
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/payment/create")
+    public CommonResult<Integer> create(Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+    }
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+}
