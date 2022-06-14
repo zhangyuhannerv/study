33de21eb3c883ccf8ee92c84449e007ead5c275f
zhangyuhan <1355166049@qq.com>
2022-06-14 14:29:13 +0800

    spring-cloud update

--- part/spring-cloud-study/cloud-eureka-server7002/src/main/java/com/study/springcloud/EurekaMain7002.java
+++ part/spring-cloud-study/cloud-eureka-server7002/src/main/java/com/study/springcloud/EurekaMain7002.java
@@ -2,12 +2,12 @@ package com.study.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer// 表示当前是eureka的服务端
+public class EurekaMain7002 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaMain7002.class, args);
     }
 }
