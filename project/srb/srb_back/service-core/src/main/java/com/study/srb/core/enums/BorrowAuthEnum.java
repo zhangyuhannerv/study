f1a8fdc88d7040360be8a4fd2aafcd75f49ada42
zhangyuhan <1355166049@qq.com>
2022-08-15 17:03:16 +0800

    srb update

---
+++ project/srb/srb_back/service-core/src/main/java/com/study/srb/core/enums/BorrowAuthEnum.java
@@ -0,0 +1,19 @@
+package com.study.srb.core.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+public enum BorrowAuthEnum {
+
+    NO_AUTH(0, "未认证"),
+    AUTH_RUN(1, "认证中"),
+    AUTH_OK(2, "认证成功"),
+    AUTH_FAIL(-1, "认证失败"),
+    ;
+
+    private Integer status;
+    private String msg;
+}
