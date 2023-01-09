46819a1d8ca13a433fbe2330da4f4edf6e34baa4
zhangyuhan <1355166049@qq.com>
2023-01-09 10:38:44 +0800

    123

--- part/kafka/kafka-async-send/src/main/java/com/study/kafka/producer/CustomProducer.java
+++ part/kafka/kafka-async-send/src/main/java/com/study/kafka/producer/CustomProducer.java
@@ -15,7 +15,7 @@ public class CustomProducer {
         // 0.配置
         Properties properties = new Properties();
         // 连接集群
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.10.43:9092");
         // 指定对应的key和value的序列化
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
@@ -23,8 +23,8 @@ public class CustomProducer {
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
 
         // 2.发送数据
-        for (int i = 0; i < 1; i++) {
-            kafkaProducer.send(new ProducerRecord<>("track_trackdata", "hello" + i));
+        for (int i = 0; i < 20; i++) {
+            kafkaProducer.send(new ProducerRecord<>("test", "hello" + i));
         }
 
         // 3.关闭资源
