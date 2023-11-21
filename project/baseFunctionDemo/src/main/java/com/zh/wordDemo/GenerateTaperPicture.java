6c42341fe6f77587fb07d09ded14ab1fd7981f83
zhangyuhan <1355166049@qq.com>
2023-11-21 17:33:52 +0800

    work demo update

--- project/baseFunctionDemo/src/main/java/com/zh/wordDemo/GenerateTaperPicture.java
+++ project/baseFunctionDemo/src/main/java/com/zh/wordDemo/GenerateTaperPicture.java
@@ -7,18 +7,17 @@ import java.util.List;
 
 public class GenerateTaperPicture {
     public static void main(String[] args) {
-//        contact_main.Class1 demo1 = new contact_main.Class1();
         String file1 = "/Users/zhangyuhan/Work/WorkProject/installconstruction/等效锥度/Rail_L1_K23+140.txt";
-        List<List<Double>> list1 = new ArrayList<>();
+        List<Double[]> list1 = new ArrayList<>();
         readData(file1, list1);
         String file2 = "/Users/zhangyuhan/Work/WorkProject/installconstruction/等效锥度/Rail_R1_K23+140.txt";
-        List<List<Double>> list2 = new ArrayList<>();
+        List<Double[]> list2 = new ArrayList<>();
         readData(file2, list2);
         String file3 = "/Users/zhangyuhan/Work/WorkProject/installconstruction/等效锥度/Wheel_1che1zhou_L_12003.txt";
-        List<List<Double>> list3 = new ArrayList<>();
+        List<Double[]> list3 = new ArrayList<>();
         readData(file3, list3);
         String file4 = "/Users/zhangyuhan/Work/WorkProject/installconstruction/等效锥度/Wheel_1che1zhou_R_12003.txt";
-        List<List<Double>> list4 = new ArrayList<>();
+        List<Double[]> list4 = new ArrayList<>();
         readData(file4, list4);
 
         System.out.println(list1.size());
@@ -26,21 +25,22 @@ public class GenerateTaperPicture {
         System.out.println(list3.size());
         System.out.println(list4.size());
 
-//        String filePath = "";
-//        Double a = 0.025;
-//        demo1.main_conicity(10, list1.toArray(), list2.toArray(), list3.toArray(), list4.toArray(), a, filePath);
+        String filePath = "/Users/zhangyuhan/Work/WorkProject/installconstruction/等效锥度/";
+        Double a = 0.025;
+        main_conicity.Class1 demo1 = new main_conicity.Class1();
+        demo1.main_conicity(1, list1.toArray(), list2.toArray(), list3.toArray(), list4.toArray(), a, filePath);
     }
 
-    public static void readData(String file, List<List<Double>> list) {
+    public static void readData(String file, List<Double[]> list) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String text;
             String[] split;
-            List<Double> point;
+            Double[] point;
             while ((text = bufferedReader.readLine()) != null) {
                 split = text.split("\\t");
-                point = new ArrayList<>();
-                point.add(Double.parseDouble(split[0]));
-                point.add(Double.parseDouble(split[1]));
+                point = new Double[2];
+                point[0] = Double.parseDouble(split[0]);
+                point[1] = (Double.parseDouble(split[1]));
                 list.add(point);
             }
         } catch (Exception e) {
