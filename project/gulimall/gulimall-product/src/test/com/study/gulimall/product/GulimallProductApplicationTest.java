4006c0c9f6dd51579a826c30b3cdcae6c0710e09
zhangyuhan <1355166049@qq.com>
2023-12-11 15:48:00 +0800

    调试

--- project/gulimall/gulimall-product/src/test/com/study/gulimall/product/GulimallProductApplicationTest.java
+++ project/gulimall/gulimall-product/src/test/com/study/gulimall/product/GulimallProductApplicationTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GulimallProductApplication {
+public class GulimallProductApplicationTest {
     @Autowired
     BrandService brandService;
 
