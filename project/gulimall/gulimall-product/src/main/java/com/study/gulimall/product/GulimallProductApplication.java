f456d2fb12bcc08562ec64baf1736e397efe7943
zhangyuhan <1355166049@qq.com>
2023-12-11 17:41:40 +0800

    整合nacos

--- project/gulimall/gulimall-product/src/main/java/com/study/gulimall/product/GulimallProductApplication.java
+++ project/gulimall/gulimall-product/src/main/java/com/study/gulimall/product/GulimallProductApplication.java
@@ -1,11 +1,11 @@
 package com.study.gulimall.product;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@MapperScan("com.study.gulimall.product.dao")
+@EnableDiscoveryClient
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
