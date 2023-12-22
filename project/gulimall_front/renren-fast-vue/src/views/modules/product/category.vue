e4a6f6ce2bad0751e36f028ee2ea36a1dfbe4ea0
zhangyuhan <1355166049@qq.com>
2023-12-22 10:42:30 +0800

    三级菜单新增功能完成

--- project/gulimall_front/renren-fast-vue/src/views/modules/product/category.vue
+++ project/gulimall_front/renren-fast-vue/src/views/modules/product/category.vue
@@ -1,36 +1,50 @@
 <template>
-  <el-tree
-    node-key="catId"
-    show-checkbox
-    :data="menus"
-    :props="defaultProps"
-    :expand-on-click-node="false"
-    :default-expanded-keys="expandedKey"
-  >
-    <span class="custom-tree-node" slot-scope="{ node, data }">
-      <span>{{ node.label }}</span>
-      <span>
-        <el-button
-          v-if="node.level <= 2"
-          type="text"
-          size="mini"
-          @click="() => append(data)"
-        >
-          Append
-        </el-button>
-        <el-button
-          v-if="node.childNodes.length == 0"
-          type="text"
-          size="mini"
-          @click="() => remove(node, data)"
-        >
-          Delete
-        </el-button>
+  <div>
+    <el-tree
+      node-key="catId"
+      show-checkbox
+      :data="menus"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      :default-expanded-keys="expandedKey"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            Append
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            Delete
+          </el-button>
+        </span>
       </span>
-    </span>
-  </el-tree>
+    </el-tree>
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCategory">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 <script>
+
 export default {
   data() {
     return {
@@ -39,7 +53,9 @@ export default {
         children: "children",
         label: "name"
       },
-      expandedKey: []
+      expandedKey: [],
+      dialogVisible: false,
+      category: {name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 0}
     };
   },
   methods: {
@@ -47,13 +63,37 @@ export default {
       this.$http({
         url: this.$http.adornUrl("/product/category/list/tree"),
         method: "get"
-      }).then(({ data }) => {
+      }).then(({data}) => {
         this.menus = data.data;
       });
     },
     append(data) {
-      console.log(data);
+      this.dialogVisible = true;
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
+      console.log(this.category);
     },
+
+    // 添加三级分类的方法
+    addCategory() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false)
+      }).then(({data}) => {
+        this.$message({
+          message: "菜单保存成功",
+          type: "success"
+        });
+        // 关闭对话框
+        this.dialogVisible = false
+        // 刷新新的菜单
+        this.getMenus();
+        // 设置需要默认展开的菜单
+        this.expandedKey = [this.category.parentCid];
+      })
+    },
+
     remove(node, data) {
       let ids = [data.catId];
       this.$confirm(`是否删除【${data.name}】菜单`, "提示", {
@@ -66,7 +106,7 @@ export default {
             url: this.$http.adornUrl("/product/category/delete"),
             method: "post",
             data: this.$http.adornData(ids, false)
-          }).then(({ data }) => {
+          }).then(({data}) => {
             this.$message({
               message: "菜单删除成功",
               type: "success"
@@ -77,7 +117,8 @@ export default {
             this.expandedKey = [node.parent.data.catId];
           });
         })
-        .catch(() => {});
+        .catch(() => {
+        });
     }
   },
   created() {
