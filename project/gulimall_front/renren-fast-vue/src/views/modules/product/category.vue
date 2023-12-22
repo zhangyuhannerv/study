6b734365d63514a1bc8a81d7184ca88b2e20d5bd
zhangyuhan <1355166049@qq.com>
2023-12-22 14:50:36 +0800

    三级菜单修改功能完成

--- project/gulimall_front/renren-fast-vue/src/views/modules/product/category.vue
+++ project/gulimall_front/renren-fast-vue/src/views/modules/product/category.vue
@@ -20,6 +20,13 @@
             Append
           </el-button>
           <el-button
+            type="text"
+            size="mini"
+            @click="() => edit(data)"
+          >
+            Edit
+          </el-button>
+          <el-button
             v-if="node.childNodes.length == 0"
             type="text"
             size="mini"
@@ -30,15 +37,21 @@
         </span>
       </span>
     </el-tree>
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+    <el-dialog :title="dialogType" :visible.sync="dialogVisible" width="30%" :close-on-click-modal="false">
       <el-form :model="category">
         <el-form-item label="分类名称">
           <el-input v-model="category.name" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input v-model="category.productUnit" autocomplete="off"></el-input>
+        </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addCategory">确 定</el-button>
+        <el-button type="primary" @click="submitData">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -55,7 +68,19 @@ export default {
       },
       expandedKey: [],
       dialogVisible: false,
-      category: {name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 0}
+      dialogType: "",// edit,add
+      category: {
+        catId: null,
+        name: "",
+        icon: "",
+        productUnit: "",
+        // ----
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0,
+
+      }
     };
   },
   methods: {
@@ -68,10 +93,17 @@ export default {
       });
     },
     append(data) {
+      this.dialogType = 'add'
       this.dialogVisible = true;
       this.category.parentCid = data.catId;
       this.category.catLevel = data.catLevel * 1 + 1;
-      console.log(this.category);
+
+      this.category.catId = null
+      this.category.name = ""
+      this.category.icon = ""
+      this.category.productUnit = ""
+      this.category.showStatus = 1
+      this.category.sort = 0
     },
 
     // 添加三级分类的方法
@@ -92,6 +124,54 @@ export default {
         // 设置需要默认展开的菜单
         this.expandedKey = [this.category.parentCid];
       })
+    },
+
+    edit(data) {
+      this.dialogType = 'edit'
+      this.dialogVisible = true
+
+      // 发送请求获取当前节点最新的数据
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: "get",
+      }).then(({data}) => {
+        this.category.catId = data.data.catId
+        this.category.name = data.data.name
+        this.category.icon = data.data.icon
+        this.category.productUnit = data.data.productUnit
+        this.category.parentCid = data.data.parentCid
+        this.category.catLevel = data.data.catLevel
+        this.category.showStatus = data.data.showStatus
+        this.category.sort = data.data.sort
+      })
+
+    },
+
+    editCategory() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/update"),
+        method: "post",
+        data: this.$http.adornData(this.category, false)
+      }).then(({data}) => {
+        this.$message({
+          message: "菜单修改成功",
+          type: "success"
+        });
+        // 关闭对话框
+        this.dialogVisible = false
+        // 刷新新的菜单
+        this.getMenus();
+        // 设置需要默认展开的菜单
+        this.expandedKey = [this.category.parentCid];
+      })
+    },
+
+    submitData() {
+      if (this.dialogType === 'add') {
+        this.addCategory()
+      } else {
+        this.editCategory()
+      }
     },
 
     remove(node, data) {
