0e698eba747fe0f865b9b3104678d47bea484835
zhangyuhan <1355166049@qq.com>
2024-01-25 11:37:00 +0800

    后端json校验概览

--- project/gulimall/gulimall-product/src/main/java/com/study/gulimall/product/entity/BrandEntity.java
+++ project/gulimall/gulimall-product/src/main/java/com/study/gulimall/product/entity/BrandEntity.java
@@ -2,14 +2,18 @@ package com.study.gulimall.product.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-
-import java.io.Serializable;
-import java.util.Date;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 
 /**
  * 品牌
- * 
+ *
  * @author ZhangYuhan
  * @email zhangyuhannerv@gmail.com
  * @date 2023-12-08 17:06:12
@@ -17,36 +21,43 @@ import lombok.Data;
 @Data
 @TableName("pms_brand")
 public class BrandEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 品牌id
-	 */
-	@TableId
-	private Long brandId;
-	/**
-	 * 品牌名
-	 */
-	private String name;
-	/**
-	 * 品牌logo地址
-	 */
-	private String logo;
-	/**
-	 * 介绍
-	 */
-	private String descript;
-	/**
-	 * 显示状态[0-不显示；1-显示]
-	 */
-	private Integer showStatus;
-	/**
-	 * 检索首字母
-	 */
-	private String firstLetter;
-	/**
-	 * 排序
-	 */
-	private Integer sort;
+    /**
+     * 品牌id
+     */
+    @TableId
+    private Long brandId;
+    /**
+     * 品牌名
+     */
+    @NotBlank(message = "品牌名不能为空")
+    private String name;
+    /**
+     * 品牌logo地址
+     */
+    @NotBlank(message = "logo不能为空")
+    @URL(message = "logo必须是一个合法的url地址")
+    private String logo;
+    /**
+     * 介绍
+     */
+    private String descript;
+    /**
+     * 显示状态[0-不显示；1-显示]
+     */
+    private Integer showStatus;
+    /**
+     * 检索首字母
+     */
+    @NotBlank
+    @Pattern(regexp = "/^[a-zA-Z]$/", message = "检索首字母必须是一个字母")
+    private String firstLetter;
+    /**
+     * 排序
+     */
+    @Min(value = 0, message = "排序必须大于等于0")
+    @NotNull
+    private Integer sort;
 
 }
