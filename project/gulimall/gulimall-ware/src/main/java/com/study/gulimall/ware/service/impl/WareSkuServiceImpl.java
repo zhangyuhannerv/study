9be3e575d8298bce49747729283654c56ef253d5
zhangyuhan <1355166049@qq.com>
2024-04-03 15:36:32 +0800

    检索

--- project/gulimall/gulimall-ware/src/main/java/com/study/gulimall/ware/service/impl/WareSkuServiceImpl.java
+++ project/gulimall/gulimall-ware/src/main/java/com/study/gulimall/ware/service/impl/WareSkuServiceImpl.java
@@ -1,16 +1,17 @@
 package com.study.gulimall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.study.common.utils.PageUtils;
 import com.study.common.utils.Query;
-
 import com.study.gulimall.ware.dao.WareSkuDao;
 import com.study.gulimall.ware.entity.WareSkuEntity;
 import com.study.gulimall.ware.service.WareSkuService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareSkuService")
@@ -18,9 +19,16 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        LambdaQueryWrapper<WareSkuEntity> qw = new LambdaQueryWrapper<>();
+        String skuId = (String) params.get("skuId");
+        String wareId = (String) params.get("wareId");
+
+        qw.eq(StringUtils.isNotBlank(skuId), WareSkuEntity::getSkuId, skuId)
+                .eq(StringUtils.isNotBlank(wareId), WareSkuEntity::getWareId, wareId);
+
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                qw
         );
 
         return new PageUtils(page);
