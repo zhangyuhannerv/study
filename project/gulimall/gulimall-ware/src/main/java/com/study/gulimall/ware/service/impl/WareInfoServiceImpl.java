6b71ab4aed36cb4df6e3c74054f68ffbba08e707
zhangyuhan <1355166049@qq.com>
2024-04-03 15:17:02 +0800

    仓库管理查询

--- project/gulimall/gulimall-ware/src/main/java/com/study/gulimall/ware/service/impl/WareInfoServiceImpl.java
+++ project/gulimall/gulimall-ware/src/main/java/com/study/gulimall/ware/service/impl/WareInfoServiceImpl.java
@@ -1,16 +1,17 @@
 package com.study.gulimall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.study.common.utils.PageUtils;
 import com.study.common.utils.Query;
-
 import com.study.gulimall.ware.dao.WareInfoDao;
 import com.study.gulimall.ware.entity.WareInfoEntity;
 import com.study.gulimall.ware.service.WareInfoService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareInfoService")
@@ -18,9 +19,18 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        String key = (String) params.get("key");
+        LambdaQueryWrapper<WareInfoEntity> qw = new LambdaQueryWrapper<>();
+        qw.eq(StringUtils.isNotBlank(key), WareInfoEntity::getId, key)
+                .or(StringUtils.isNotBlank(key))
+                .like(StringUtils.isNotBlank(key), WareInfoEntity::getName, key)
+                .or(StringUtils.isNotBlank(key))
+                .like(StringUtils.isNotBlank(key), WareInfoEntity::getAddress, key)
+                .or(StringUtils.isNotBlank(key))
+                .like(StringUtils.isNotBlank(key), WareInfoEntity::getAreacode, key);
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
+                qw
         );
 
         return new PageUtils(page);
