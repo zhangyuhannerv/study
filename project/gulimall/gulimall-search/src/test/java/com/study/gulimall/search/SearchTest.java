d6c122e1ae94fd3d4f9b6d5f75c5449e2087fc15
zhangyuhan <1355166049@qq.com>
2024-04-29 11:34:05 +0800

    整合els hight level client

---
+++ project/gulimall/gulimall-search/src/test/java/com/study/gulimall/search/SearchTest.java
@@ -0,0 +1,17 @@
+package com.study.gulimall.search;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SearchTest {
+    @Autowired
+    RestHighLevelClient client;
+
+    @Test
+    public void contextLoads() {
+        System.out.println(client);
+    }
+}
