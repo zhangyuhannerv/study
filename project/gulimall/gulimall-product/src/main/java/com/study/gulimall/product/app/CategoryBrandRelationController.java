c504f3a48a9e31fc26cdc44dd859a8ff45f691cb
zhangyuhan <1355166049@qq.com>
2024-05-10 14:10:59 +0800

    引入商品页面thymeleaf

---
+++ project/gulimall/gulimall-product/src/main/java/com/study/gulimall/product/app/CategoryBrandRelationController.java
@@ -0,0 +1,112 @@
+package com.study.gulimall.product.app;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.study.common.utils.PageUtils;
+import com.study.common.utils.R;
+import com.study.gulimall.product.entity.BrandEntity;
+import com.study.gulimall.product.entity.CategoryBrandRelationEntity;
+import com.study.gulimall.product.service.CategoryBrandRelationService;
+import com.study.gulimall.product.vo.BrandVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+/**
+ * 品牌分类关联
+ *
+ * @author ZhangYuhan
+ * @email zhangyuhannerv@gmail.com
+ * @date 2023-12-08 17:31:23
+ */
+@RestController
+@RequestMapping("product/categorybrandrelation")
+public class CategoryBrandRelationController {
+    @Autowired
+    private CategoryBrandRelationService categoryBrandRelationService;
+
+    /**
+     * 列表
+     */
+    @RequestMapping("/list")
+    // @RequiresPermissions("product:categorybrandrelation:list")
+    public R list(@RequestParam Map<String, Object> params) {
+        PageUtils page = categoryBrandRelationService.queryPage(params);
+
+        return R.ok().put("page", page);
+    }
+
+    /**
+     * 获取当前品牌关联的所有列表分类
+     */
+    @GetMapping("/catelog/list")
+    public R catelogList(Long brandId) {
+        LambdaQueryWrapper<CategoryBrandRelationEntity> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(CategoryBrandRelationEntity::getBrandId, brandId);
+        List<CategoryBrandRelationEntity> data =
+                categoryBrandRelationService.list(queryWrapper);
+        return R.ok().put("data", data);
+    }
+
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{id}")
+    // @RequiresPermissions("product:categorybrandrelation:info")
+    public R info(@PathVariable("id") Long id) {
+        CategoryBrandRelationEntity categoryBrandRelation = categoryBrandRelationService.getById(id);
+
+        return R.ok().put("categoryBrandRelation", categoryBrandRelation);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    // @RequiresPermissions("product:categorybrandrelation:save")
+    public R save(@RequestBody CategoryBrandRelationEntity categoryBrandRelation) {
+        categoryBrandRelationService.saveDetail(categoryBrandRelation);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    // @RequiresPermissions("product:categorybrandrelation:update")
+    public R update(@RequestBody CategoryBrandRelationEntity categoryBrandRelation) {
+        categoryBrandRelationService.updateById(categoryBrandRelation);
+
+        return R.ok();
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    // @RequiresPermissions("product:categorybrandrelation:delete")
+    public R delete(@RequestBody Long[] ids) {
+        categoryBrandRelationService.removeByIds(Arrays.asList(ids));
+
+        return R.ok();
+    }
+
+    @GetMapping("/brands/list")
+    public R relationBrandsList(Long catId) {
+        List<BrandEntity> vos = categoryBrandRelationService.getBrandsByCatId(catId);
+        List<BrandVo> collect = vos.stream().map(item -> {
+            BrandVo brandVo = new BrandVo();
+            brandVo.setBrandId(item.getBrandId());
+            brandVo.setBrandName(item.getName());
+            return brandVo;
+        }).collect(Collectors.toList());
+        return R.ok().put("data", collect);
+    }
+
+}
---
+++ project/gulimall/gulimall-product/src/main/java/com/study/gulimall/product/app/CategoryController.java
@@ -0,0 +1,95 @@
+package com.study.gulimall.product.app;
+
+import com.study.common.utils.R;
+import com.study.gulimall.product.entity.CategoryEntity;
+import com.study.gulimall.product.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * 商品三级分类
+ *
+ * @author ZhangYuhan
+ * @email zhangyuhannerv@gmail.com
+ * @date 2023-12-08 17:31:23
+ */
+@RestController
+@RequestMapping("product/category")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 查出所有分类以及子分类，以树形结构组装起来
+     */
+    @RequestMapping("/list/tree")
+    // @RequiresPermissions("product:category:list")
+    public R list() {
+        List<CategoryEntity> entities = categoryService.listWithTree();
+        return R.ok().put("data", entities);
+    }
+
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{catId}")
+    // @RequiresPermissions("product:category:info")
+    public R info(@PathVariable("catId") Long catId) {
+        CategoryEntity category = categoryService.getById(catId);
+
+        return R.ok().put("data", category);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    // @RequiresPermissions("product:category:save")
+    public R save(@RequestBody CategoryEntity category) {
+        categoryService.save(category);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    // @RequiresPermissions("product:category:update")
+    public R update(@RequestBody CategoryEntity category) {
+        categoryService.updateCascade(category);
+
+        return R.ok();
+    }
+
+    /**
+     * 批量修改
+     */
+    @RequestMapping("/update/sort")
+    // @RequiresPermissions("product:category:update")
+    public R update(@RequestBody CategoryEntity[] category) {
+        categoryService.updateBatchById(Arrays.asList(category));
+        return R.ok();
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    // @RequiresPermissions("product:category:delete")
+    public R delete(@RequestBody Long[] catIds) {
+
+        categoryService.removeMenuByIds(Arrays.asList(catIds));
+
+        return R.ok();
+    }
+
+}
