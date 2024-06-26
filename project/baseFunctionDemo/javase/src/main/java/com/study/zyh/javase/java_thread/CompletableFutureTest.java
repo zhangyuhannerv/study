7e7dad48e1e0046ecdc9af1d250ccc13b54c7424
zhangyuhan <1355166049@qq.com>
2024-06-26 10:47:46 +0800

    CompletableFuture

--- project/baseFunctionDemo/javase/src/main/java/com/study/zyh/javase/java_thread/CompletableFutureTest.java
+++ project/baseFunctionDemo/javase/src/main/java/com/study/zyh/javase/java_thread/CompletableFutureTest.java
@@ -10,6 +10,7 @@ public class CompletableFutureTest {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         System.out.println("main...start");
+        // 异步执行
 //        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
 //            System.out.println("当前线程:" + Thread.currentThread().getId());
 //            int i = 10 / 2;
@@ -18,17 +19,55 @@ public class CompletableFutureTest {
 //        System.out.println("main...end");
 
 
+        // 阻塞当前线程异步执行
+//        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+//            System.out.println("当前线程:" + Thread.currentThread().getId());
+//            int i = 10 / 2;
+//            System.out.println("运行结果:" + i);
+//            return i;
+//        }, service);
+//
+//        // 不调用get()就会异步执行,先打印end
+//        // 调用get()就会阻塞当前线程,先打印运行结果
+//        Integer i = future.get();
+//        System.out.println("main...end..." + i);
+
+        // 链式调用1：whenComplete,方法完成后的感知
+//        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+//            System.out.println("当前线程:" + Thread.currentThread().getId());
+//            int i = 10 / 0;
+////            int i = 10 / 2;
+//            System.out.println("运行结果:" + i);
+//            return i;
+//        }, service).whenComplete((res, exception) -> {
+//            // 这里可以感知到任务结束执行了
+//            // 虽然能得到异常信息，但是没办法修改返回数据,只能继续执行新的任务
+//            System.out.println("异步任务执行结束了,结果是：" + res + ",异常是:" + exception);
+//        }).exceptionally(exception -> {
+//            // 这里也可以感知到异常，同时返回默认值
+//            // 结果出现异常之后，就给个默认返回
+//            System.out.println("感知到的异常：" + exception);
+//            return 10;
+//        });
+//
+//        Integer i = future.get();
+//        System.out.println("main...end..." + i);
+
+        // 链式调用2：handle，方法完成后的处理
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
             System.out.println("当前线程:" + Thread.currentThread().getId());
-            int i = 10 / 2;
+            int i = 10 / 0;
+//            int i = 10 / 2;
             System.out.println("运行结果:" + i);
             return i;
-        }, service);
+        }, service).handle((res, exception) -> {
+            if (exception != null) {
+                return 0;
+            }
+            return res * 2;
+        });
 
-        // 不调用get()就会异步执行,先打印end
-        // 调用get()就会阻塞当前线程,先打印运行结果
         Integer i = future.get();
         System.out.println("main...end..." + i);
-
     }
 }
